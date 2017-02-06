01f0b07858fab36e291c0585015fd7c7183baa66
Thiruch Rakthao <sax_inw_com@hotmail.com>
2017-02-06 15:40:08 +0700

    Add GameManager Object & Script

---
+++ Leap/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds (startWait);
+		while(true)
+		{
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+//			if (gameOver) {
+//				restartText.text = "Press R for restart";
+//				restart = true;
+//				break;
+//			}
+		}
+	}
+}
