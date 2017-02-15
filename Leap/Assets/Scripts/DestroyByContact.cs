d323abb71ff42cbc833740d48817bcf7003b7c58
Thiruch Rakthao <sax_inw_com@hotmail.com>
2017-02-15 16:30:52 +0700

    Object Controller: Add Playable Area & Destroy Object If Outside Area

---
+++ Leap/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	// public GameObject playerExplosion;
+	public int scoreValue;
+	private GameManager gameManager;
+
+	void Start()
+	{
+		GameObject gameManagerObject = GameObject.FindWithTag ("GameManager");
+		if (gameManagerObject != null) {
+			gameManager = gameManagerObject.GetComponent<GameManager> ();
+			Debug.Log ("Find");
+		} else {
+			Debug.Log ("Cannot Find");
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary") {
+			return ;
+		}
+		// Debug.Log ("Non-Boundary");
+		Instantiate(explosion, transform.position, transform.rotation);
+		// if (other.tag == "Player") {
+		// 	Instantiate(playerExplosion, other.transform.position, other.transform.rotation) ;
+		// 	gameManager.GameOver ();
+		// }
+		// gameManager.AddScore (scoreValue);
+		// Destroy (other.gameObject);
+		// Destroy (gameObject);
+	}
+}
